bece633c9b920f763d50a17325f486f2b0273344
ShEvVl <shornikov.ev.vl@gmail.com>
2022-02-03 21:23:16 +0300

    raw endform

--- main.py
+++ main.py
@@ -1,58 +1,58 @@
-from flask import Flask, render_template, request
-import pandas as pd
+import random
 
+import pandas as pd
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/request', methods=['GET', 'POST'])
-def form():
-    # handle the POST request
-    if request.method == 'POST':
-        category1 = request.form.get('category1')
-        category2 = request.form.get('category2')
-        category3 = request.form.get('category3')
-        category4 = request.form.get('category4')
-        category5 = request.form.get('category5')
-        category6 = request.form.get('category6')
-        category7 = request.form.get('category7')
-        category8 = request.form.get('category8')
-        category9 = request.form.get('category9')
-        category10 = request.form.get('category10')
-        return '''<h1>You choose: <br> {}.</h1><br>
-        <image src = "http://localhost:8080/static/image1.jpg">'''.format(', '.join([
-            category1,
-            category2,
-            category3,
-            category4,
-            category5,
-            category6,
-            category7,
-            category8,
-            category9,
-            category10
-        ]))
-
-    return '''<h1>Choose several categories from the list: <br> {}</h1>
-            <form method="POST">
-            Category1 <input type="text" name="category1"><br>            
-            Category2 <input type="text" name="category2"><br> 
-            Category3 <input type="text" name="category3"><br> 
-            Category4 <input type="text" name="category4"><br> 
-            Category5 <input type="text" name="category5"><br> 
-            Category6 <input type="text" name="category6"><br> 
-            Category7 <input type="text" name="category7"><br> 
-            Category8 <input type="text" name="category8"><br> 
-            Category9 <input type="text" name="category9"><br> 
-            Category10 <input type="text" name="category10"><br> 
-            <input type="submit"><br> 
-            </form>'''.format(', '.join(categories))
+@app.route('/request')
+def Request():
+    return render_template(
+        'Request.html',
+        data=[{'name': req[0]},
+              {'name': req[1]},
+              {'name': req[2]},
+              {'name': req[3]},
+              {'name': req[4]},
+              {'name': req[5]},
+              {'name': req[6]},
+              {'name': req[7]},
+              {'name': req[8]},
+              {'name': req[9]},
+              {'name': req[10]},
+              {'name': req[11]},
+              {'name': req[12]},
+              {'name': req[13]},
+              {'name': req[14]},
+              {'name': req[15]},
+              {'name': req[16]},
+              {'name': req[17]},
+              {'name': req[18]},
+              {'name': req[19]}])
 
 
-@app.route('/response')
-def response():
-    return render_template('response.html')
+@app.route("/response", methods=['GET', 'POST'])
+def Response():
+    select1 = request.form.get('comp_select1')
+    select2 = request.form.get('comp_select2')
+    select3 = request.form.get('comp_select3')
+    select4 = request.form.get('comp_select4')
+    select5 = request.form.get('comp_select5')
+    select6 = request.form.get('comp_select6')
+    select7 = request.form.get('comp_select7')
+    select8 = request.form.get('comp_select8')
+    select9 = request.form.get('comp_select9')
+    select10 = request.form.get('comp_select10')
+    select_all = [select1, select2, select3, select4, select5, select6, select7, select8, select9, select10]
+    select = []
+    for i in select_all:
+        if i != None:
+            select.append(i)
+        else:
+            pass
+    return render_template('Response.html', data=[{'output': output(select)[0], 'input': select, 'count': output(select)[2]}])
 
 
 with open('info.csv', 'r') as f:
@@ -63,7 +63,7 @@ for i in range(len(source)):
     data.append(source[i].replace("\n", ""))
 df = pd.Series(data).str.split(pat=";", expand=True)
 df.columns = ["Path",
-              "needed_amount_of_shows",
+              "amount_of_shows",
               "category1",
               "category2",
               "category3",
@@ -75,27 +75,51 @@ df.columns = ["Path",
               "category9",
               "category10"
               ]
+df['amount_of_shows'] = df['amount_of_shows'].astype(int)
+df['amount_of_shows'] = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
 print(df)
-categories = ('apple',
-              'banana',
-              'city',
-              'drill',
-              'ear',
-              'forest',
-              'gim',
-              'hill',
-              'ink',
-              'juice',
-              'koala',
-              'lion',
-              'mouse',
-              'notebook',
-              'octopus',
-              'panda',
-              'queen',
-              'rabbit',
-              'sun',
-              'tea')
+
+req = list(set(df.iloc[:, 2:].values.flatten()))
+print(req)
+log_ind = []
+
+
+def output(r):
+    seq = []
+    if r == []:
+        top_seq_path = df.iloc[random.randint(0, len(df) - 1), 0]
+        ind_top = df.index[df['Path'] == top_seq_path].tolist()
+        log_ind.append(ind_top[0])
+    else:
+        for i in range(len(r)):
+            for k in range(10):
+                c = df.index[df[f'category{k + 1}'] == r[i]].tolist()
+                if c == []:
+                    pass
+                else:
+                    seq.append(c)
+        seq_u = list(set([item for sublist in seq for item in sublist]))
+        df_t = df.iloc[seq_u].sort_values(by=['amount_of_shows'], ascending=False)
+        top_seq_path = df_t.iloc[0, 0]
+        ind_top = df.index[df['Path'] == top_seq_path].tolist()
+        if log_ind == []:
+            pass
+        else:
+            if ind_top[0] == log_ind[-1]:
+                rand_ind = random.randint(0, len(df_t) - 1)
+                while log_ind[-1] == rand_ind:
+                    rand_ind = random.randint(0, len(df_t) - 1)
+                else:
+                    top_seq_path = df_t.iloc[rand_ind, 0]
+                    ind_top = df.index[df['Path'] == top_seq_path].tolist()
+        log_ind.append(ind_top[0])
+        df.iloc[ind_top, 1] = df.iloc[ind_top, 1] - 1
+        if df.iloc[ind_top, 1].values[0] > 0:
+            pass
+        else:
+            top_seq_path = ''
+    return top_seq_path, log_ind, df.iloc[ind_top, 1].values[0]
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
